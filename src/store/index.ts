cab27962130962adcc11441594cd9cdb68e75f2e
Mike <mike@bowerbird.io>
2023-08-24 14:35:35 +0800

    export store root state type

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoSliceReducer from "./todoSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     todoList: todoSliceReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>
+
+export default store
